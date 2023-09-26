131193a902dae027172e6d81d3a30d3adf46d6ad
Daniela Rocha Silva <danirocha.rmd@gmail.com>
2023-09-26 18:03:56 -0300

    feat: gets intro from wikipedia

--- main.go
+++ main.go
@@ -1,23 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+type Page struct {
+	Extract string
+}
+
+type Data struct {
+	Pages []Page
+}
+
+type Location struct {
+	Name  string
+	Intro string
+}
+
+func getIntro(location string) string {
+	url := "https://en.wikipedia.org/api/rest_v1/page/related/"
+	re := regexp.MustCompile(`\s+`)
+	url += re.ReplaceAllString(location, "_")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var d Data
+	if err := json.Unmarshal(data, &d); err != nil {
+		log.Fatal(err)
+	}
+
+	return string(d.Pages[0].Extract)
+}
 
 func FindAllAbout(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		log.Fatalf("An error occurred: %v", err)
+		log.Fatal(err)
 		return
 	}
 
 	location := r.FormValue("location")
 	fmt.Println("SEARCH ALL ABOUT ", location)
+	intro := getIntro(location)
 
-	io.WriteString(w, location)
+	s, err := json.Marshal(Location{location, intro})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.WriteString(w, string(s))
 }
 
 func main() {
